0e1eef38d88703ec891f15a4c71b2d6c2afa63f8
Zean23 <100299651+Zean23@users.noreply.github.com>
2022-03-11 10:57:02 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
